a1d52feca8ec4048466b447a0b44ac9042a9c07d
changsoon123 <sool0458@naver.com>
2023-04-30 16:17:54 +0900

    price 완료

--- project_sql/src/com/java/view/StartUI.java
+++ project_sql/src/com/java/view/StartUI.java
@@ -50,7 +50,8 @@ public class StartUI {
 			System.out.println("\u001B[31;1;4;41;42;43;44;45;46m----------------------------------------------------\u001B[0m");
 	        System.out.println("1.전체 검색");
 	        System.out.println("2.국가별 맛집 검색(한식,중식,일식,양식)");
-	        System.out.println("3.지역별 맛집 검색(서울,인천,경기도,강원도,충청도,경상도,전라도)");	        
+	        System.out.println("3.지역별 맛집 검색(서울,인천,경기도,강원도,충청도,경상도,전라도)");	  
+	        System.out.println("4.가격별 맛집 검색(만원 미만,2만원 미만,3만원 미만,4만원 미만,5만원 미만,5만원 이상)");	  
 	        System.out.print("\u001B[31;1;4;41;42;43;44;45;46m >>> 원하는 숫자를 눌러주세요 :\u001B[0m");
 		}
 		
@@ -72,6 +73,17 @@ public class StartUI {
 			System.out.println("5.충청도 검색");
 			System.out.println("6.경상도 검색");
 			System.out.println("7.전라도 검색");
+			System.out.print("\u001B[31;1;4;41;42;43;44;45;46m >>> 원하는 숫자를 눌러주세요 :\u001B[0m");
+		}
+		
+		public static void pricethen() {
+			System.out.println("\u001B[31;1;4;41;42;43;44;45;46m----------------------------------------------------\u001B[0m");
+			System.out.println("1.0 ~ 9900원 검색");
+			System.out.println("2.10000 ~ 19900원 검색");
+			System.out.println("3.20000 ~ 29900원 검색");
+			System.out.println("4.30000 ~ 39900원 검색");
+			System.out.println("5.40000 ~ 49900원 검색");
+			System.out.println("6.50000원 이상 검색");
 			System.out.print("\u001B[31;1;4;41;42;43;44;45;46m >>> 원하는 숫자를 눌러주세요 :\u001B[0m");
 		}
 		
